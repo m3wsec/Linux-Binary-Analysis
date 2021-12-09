f5903e98b652202910ea0801da72d2161bfb88b0
m3w <72904943+m3wsec@users.noreply.github.com>
2021-12-09 12:26:00 +0800

    Update shellcode_jmp.c

--- asm/shellcode_jmp.c
+++ asm/shellcode_jmp.c
@@ -4,9 +4,7 @@ void _start()
     //absolute address jmp operand can not be immediate
     __asm__ __volatile__
     (
-        "push %eax              \n\t"
         "mov $0x8049080,%eax    \n\t"
         "jmp *%eax              \n\t"
-        "pop %eax               \n\t"
     );
 }
